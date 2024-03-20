f1af57ae294f43e3763a874c7ca9d95a8588ae9a
Joseph Cavanna <75166493+josephcavanna@users.noreply.github.com>
2024-03-20 12:54:17 +0100

    minor fixes

--- lib/screens/account_page.dart
+++ lib/screens/account_page.dart
@@ -20,7 +20,7 @@ class _AccountPageState extends State<AccountPage> {
   final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
   final languageSelection = LanguageSelection();
   final version = 'v1.0.1';
-  final privacyPolicyUrl = 'https://www.privacypolicies.com/live/f0bc7c1e-176e-453b-b250-7d62bf808a3b';
+  final privacyPolicyUrl = 'https://github.com/josephcavanna/bedtime_story_ai/blob/3a59cb00445daf21c7e08295a7b782cf45643d0e/privacy-policy.md';
   String? language;
   bool isReset = false;
 
